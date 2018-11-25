5a3a47f8eac1c14e747fc770aa834d19153abcfe
Brett Hartman <brett.hartman@mac.com>
2018-11-25 11:39:32 -0800

    Delete most of create-react-app scaffolding

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
+ReactDOM.render(
+	<div>
+		<h1>Tic-Tac-Turing</h1>
+	</div>,
+	document.getElementById('root')
+);
